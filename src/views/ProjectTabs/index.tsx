29f5742d244a9e8092211f18a32828f352032fac
xichen <andxic@163.com>
2022-03-20 16:14:26 +0800

    add: projectTabs

---
+++ src/views/ProjectTabs/index.tsx
@@ -0,0 +1,17 @@
+/*
+*  Create by xic, date: 2022/3/20
+*      _   _        .      .                         ___           |"|          _   _     
+*     (_)-(_)     .  .:::.            ,,,,,         .|||.         _|_|_        '\\-//`    
+*      (o o)        :(o o):  .       /(o o)\        (o o)         (o o)         (o o)     
+*  ooO--(_)--Ooo-ooO--(_)--Ooo----ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-
+*/
+import React from 'react';
+
+const ProjectTabs = function(){
+    return (
+        <div>
+            123
+        </div>
+    )
+}
+export default ProjectTabs
