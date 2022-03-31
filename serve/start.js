f6eab27af5a5be041b9c2786558ccab3ce1e4290
wangjianhua <wangjianhua@qmai.cn>
2022-03-31 16:27:28 +0800

    upd:更新start脚本

---
+++ serve/start.js
@@ -0,0 +1,12 @@
+// 项目启动
+const shell = require('shelljs')
+function start(path) {
+    const children = shell.exec('npm install && npm run dev',{
+        cwd:path,
+        async:true
+    })
+    children.stdout.on('data',function(data){
+        shell.echo(data).to('log.txt')
+    })
+}
+export default start
