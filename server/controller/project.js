02d4c088ed6f2b5b35eadb265deb4ff218384253
wangjianhua <wangjianhua@qmai.cn>
2022-05-23 18:59:35 +0800

    upd:新增项目名称 pid

--- server/controller/project.js
+++ server/controller/project.js
@@ -1,21 +1,37 @@
 /*
-*  Create by xic, date: 2022/5/22
-*               _            _
-*              (_)          | |
-*      __  __  _      ___  | |__     ___   _ __
-*     \ \/ / | |    / __| | '_ \   / _ \ | '_ \
-*     >  <  | |   | (__  | | | | |  __/ | | | |
-*   /_/\_\ |_|    \___| |_| |_|  \___| |_| |_|
-*/
+ *  Create by xic, date: 2022/5/22
+ *               _            _
+ *              (_)          | |
+ *      __  __  _      ___  | |__     ___   _ __
+ *     \ \/ / | |    / __| | '_ \   / _ \ | '_ \
+ *     >  <  | |   | (__  | | | | |  __/ | | | |
+ *   /_/\_\ |_|    \___| |_| |_|  \___| |_| |_|
+ */
+
+const {
+	createNewProject,
+	creatNewProjectName,
+	insertProject,
+} = require('../services/projectService')
 const { geFileListSize, getScript } = require('../utils')
-async function createNewProjectByImport(path){
-    const sizes = geFileListSize(path)
-    const scripts = await getScript(path)
-    return {
-        sizes,
-        scripts,
-    }
+async function createNewProjectByImport(path) {
+	const sizes = geFileListSize(path)
+	const scripts = await getScript(path)
+	const name = creatNewProjectName(path)
+	const pid = createNewProject()
+	/**储存 */
+	// insertProject({
+	// 	name,
+	// 	pid,
+	// 	sizes,
+	// })
+	return {
+		name,
+		pid,
+		sizes,
+		scripts,
+	}
 }
 module.exports = {
-    createNewProjectByImport
+	createNewProjectByImport,
 }
--- server/services/projectService.js
+++ server/services/projectService.js
@@ -1,20 +1,43 @@
 /*
-*  Create by xic, date: 2022/5/22
-*               _            _
-*              (_)          | |
-*      __  __  _      ___  | |__     ___   _ __
-*     \ \/ / | |    / __| | '_ \   / _ \ | '_ \
-*     >  <  | |   | (__  | | | | |  __/ | | | |
-*   /_/\_\ |_|    \___| |_| |_|  \___| |_| |_|
-*/
-const { v4: uuidv4 } = require('uuid');
-function createNewProject(path){
-   return uuidv4()
+ *  Create by xic, date: 2022/5/22
+ *               _            _
+ *              (_)          | |
+ *      __  __  _      ___  | |__     ___   _ __
+ *     \ \/ / | |    / __| | '_ \   / _ \ | '_ \
+ *     >  <  | |   | (__  | | | | |  __/ | | | |
+ *   /_/\_\ |_|    \___| |_| |_|  \___| |_| |_|
+ */
+const { v4: uuidv4 } = require('uuid')
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database(':memory:')
+/**
+ *
+ * 创建项目生成pid
+ * @returns uuid
+ */
+function createNewProject() {
+	return uuidv4()
 }
-function queryProjectByPid(path){
-    return null
+/**
+ * 创建项目生成name
+ * @param {*} path
+ * @returns name
+ */
+function creatNewProjectName(path) {
+	return path.split('/').pop()
+}
+function queryProjectByPid(path) {
+	return null
+}
+function insertProject(projectInfo) {
+	db.run('UPDATE tbl SET name = $name WHERE id = $id', {
+		$id: projectInfo.pid,
+		$name: projectInfo.name,
+	})
 }
 module.exports = {
-    createNewProject,
-    queryProjectByPid
+	createNewProject,
+	queryProjectByPid,
+	creatNewProjectName,
+	insertProject,
 }
