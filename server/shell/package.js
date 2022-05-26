ef5f32f56a14a48c71cf070a3dede4675d67dd42
wangjianhua <wangjianhua@qmai.cn>
2022-05-26 10:15:49 +0800

    add:todo获取包版本

--- server/shell/package.js
+++ server/shell/package.js
@@ -7,12 +7,13 @@ const INSTALL_UPDATE = ['install', 'update']
 /**
  * 更新或者安装包的公共方法
  * @param {
- * 0  1
+ * 0:install
+ * 1:update
  * } type
  * @param {
- * pid
- * packageName
- * version
+ * pid:唯一标识
+ * packageName:包名称
+ * version:包版本
  * } params
  * @param {
  *
@@ -37,9 +38,9 @@ function installOrUpdatePackage(type, params, callback) {
 /**
  *安装包
  * @param {
- * pid
- * packageName
- * version
+ * pid:唯一标识
+ * packageName:包名称
+ * version:包版本
  * } params
  * @param {
  *
@@ -52,9 +53,9 @@ function installPackage(params, callback) {
 /**
  *更新包
  * @param {
- * pid
- * packageName
- * version
+ * pid:唯一标识
+ * packageName:包名称
+ * version:包版本
  * } params
  * @param {
  *
@@ -63,7 +64,38 @@ function installPackage(params, callback) {
 function updatePackage(params, callback) {
 	installOrUpdatePackage(1, params, callback)
 }
+/**
+ *更新包
+ * @param {
+ * pid:唯一标识
+ * packageName:包名称
+ * version:包版本
+ * } params
+ * @param {
+ *
+ * } callback
+ */
+function getPackageVersionsByNpm(params, callback) {
+	const { pid, packageName, version = 'latest' } = params
+	const path = queryProject(pid)?.path
+	shell.cd(path)
+	const child = shell.exec(`npm view ${packageName} versions --json`, {
+		silent: true,
+		async: true,
+	})
+	const arr = []
+	child.stdout.on('data', function (code, stdout, stderr) {
+		const versions = code.split(',\n')
+
+		console.log(11111, versions)
+		callback?.(code, stdout, stderr)
+	})
+}
 module.exports = {
 	installPackage,
 	updatePackage,
 }
+getPackageVersionsByNpm({
+	pid: '46b54b41-295d-48e2-8e3a-6648897f904f',
+	packageName: 'react',
+})
